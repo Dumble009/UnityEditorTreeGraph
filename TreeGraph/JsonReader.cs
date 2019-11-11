033e9b6ca5ffe7ef69030f3be316daf9db7a834d
Dumble009 <daichi19990821@gmail.com>
2019-11-11 16:55:29 +0900

    MakeJsonReader

---
+++ TreeGraph/JsonReader.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+class JsonReader
+{
+	private CodeTemplateHolder templateHolder;
+	public JsonReader()
+	{
+		using (var fs = new StreamReader(Application.dataPath + "/BehaviourTree/TreeGraph/CodeTemplateTable.json"))
+		{
+			string str = fs.ReadToEnd();
+			byte[] data = System.Text.Encoding.Unicode.GetBytes(str);
+			using (var ms = new MemoryStream(data))
+			{
+				using (var sr = new StreamReader(ms))
+				{
+					var serializer = new DataContractJsonSerializer(typeof(CodeTemplateHolder));
+
+					ms.Position = 0;
+
+					templateHolder = (CodeTemplateHolder)serializer.ReadObject(ms);
+				}
+			}
+		}
+	}
+
+	public string GetDeclare(string key)
+	{
+		return templateHolder.CodeTemplatesDeclare[key];
+	}
+
+	public string GetInit(string key)
+	{
+		return templateHolder.CodeTemplatesInit[key];
+	}
+}
+
+public class CodeTemplateHolder
+{
+	public Dictionary<string, string> CodeTemplatesDeclare { get; set; }
+	public Dictionary<string, string> CodeTemplatesInit { get; set; }
+}
