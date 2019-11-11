2b8417bc22b306dc17ee7892d7c9adefe6b68a8e
Dumble009 <daichi19990821@gmail.com>
2019-11-11 17:45:28 +0900

    Complete Change CodeTemplateSystem

--- TreeGraph/CodeTemplateReader.cs
+++ TreeGraph/CodeTemplateReader.cs
@@ -5,8 +5,7 @@ public class CodeTemplateReader
 {
 	private CodeTemplateReader()
 	{
-		dirName = Application.dataPath + "/BehaviourTree/TreeGraph/CodeTemplates/";
-		jsonReader = new JsonReader();
+		dirName = Application.dataPath + "/BehaviourTree/TreeGraph/CodeTemplates";
 	}
 
 	private static CodeTemplateReader instance;
@@ -20,13 +19,16 @@ public class CodeTemplateReader
 		}
 	}
 	string dirName;
-	private JsonReader jsonReader;
 
 	public string GetDeclareTemplate(string key)
 	{
+		JsonReader jsonReader = new JsonReader();
 		string template = "";
 		string fileName = jsonReader.GetDeclare(key);
+		Debug.Log(dirName);
+		Debug.Log(fileName);
 		string path = Path.Combine(dirName, fileName);
+		Debug.Log(path);
 		using (var str = new StreamReader(path))
 		{
 			template = str.ReadToEnd();
@@ -36,6 +38,7 @@ public class CodeTemplateReader
 
 	public string GetInitTemplate(string key)
 	{
+		JsonReader jsonReader = new JsonReader();
 		string template = "";
 		string fileName = jsonReader.GetInit(key);
 		string path = Path.Combine(dirName, fileName);
