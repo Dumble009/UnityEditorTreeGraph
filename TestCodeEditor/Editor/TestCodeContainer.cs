fd2d0d8de37806ec0c84dad083f756cf1785b0b6
Dumble009 <daichi19990821@gmail.com>
2019-12-08 01:52:11 +0900

    Create Window And Container Templates

---
+++ TestCodeEditor/Editor/TestCodeContainer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "TestCodeContainer", menuName = "TestCodeContainer", order = 0)]
+public class TestCodeContainer : ScriptableObject
+{
+
+}
---
+++ TestCodeEditor/Editor/TestCodeEditorWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TestCodeEditorWindow : EditorWindow
+{
+	[UnityEditor.Callbacks.OnOpenAsset(0)]
+	public static bool Open(int instanceID, int line)
+	{
+		TestCodeContainer container = EditorUtility.InstanceIDToObject(instanceID) as TestCodeContainer;
+		if (container != null)
+		{
+			Open(container);
+			return true;
+		}
+
+		return false;
+	}
+
+	public static void Open(TestCodeContainer container)
+	{
+		if (container == null)
+		{
+			return;
+		}
+
+		TestCodeEditorWindow w = GetWindow(typeof(TestCodeEditorWindow), false, "TestCodeEditor", true) as TestCodeEditorWindow;
+	}
+}
