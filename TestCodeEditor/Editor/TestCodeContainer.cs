ac642f05b3c758f773db59577c4968a50e9e49b9
Dumble009 <daichi19990821@gmail.com>
2019-12-09 22:01:21 +0900

    Make New TestCase Area

--- TestCodeEditor/Editor/TestCodeContainer.cs
+++ TestCodeEditor/Editor/TestCodeContainer.cs
@@ -1,8 +1,24 @@
 ﻿using UnityEngine;
-using UnityEditor;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "TestCodeContainer", menuName = "TestCodeContainer", order = 0)]
 public class TestCodeContainer : ScriptableObject
 {
+	[SerializeField]
+	EditorTreeGraph treeGraph;
+	public EditorTreeGraph TreeGraph {
+		get {
+			return treeGraph;
+		}
+	}
 
+	List<TestCase> testCases;
+	public List<TestCase> TestCases {
+		get {
+			return testCases;
+		}
+		set {
+			testCases = value;
+		}
+	}
 }
--- TestCodeEditor/Editor/TestCodeEditorWindow.cs
+++ TestCodeEditor/Editor/TestCodeEditorWindow.cs
@@ -3,11 +3,13 @@ using UnityEditor;
 
 public class TestCodeEditorWindow : EditorWindow
 {
+	TestCodeContainer targetContainer;
+
 	[UnityEditor.Callbacks.OnOpenAsset(0)]
 	public static bool Open(int instanceID, int line)
 	{
 		TestCodeContainer container = EditorUtility.InstanceIDToObject(instanceID) as TestCodeContainer;
-		if (container != null)
+		if (container != null && container.TreeGraph != null)
 		{
 			Open(container);
 			return true;
@@ -18,11 +20,34 @@ public class TestCodeEditorWindow : EditorWindow
 
 	public static void Open(TestCodeContainer container)
 	{
-		if (container == null)
+		if (container == null || container.TreeGraph == null)
 		{
 			return;
 		}
 
 		TestCodeEditorWindow w = GetWindow(typeof(TestCodeEditorWindow), false, "TestCodeEditor", true) as TestCodeEditorWindow;
+		w.targetContainer = container;
+	}
+
+	private void OnGUI()
+	{
+		Rect newTestCaseArea = new Rect(10, 10, this.position.width / 3.0f, this.position.height / 3.0f);
+		Draw_NewTestCase(newTestCaseArea);
+	}
+
+	string newTestCaseName = "";
+	private void Draw_NewTestCase(Rect area)
+	{
+		GUILayout.BeginArea(area);
+		GUIStyle titleStyle = new GUIStyle()
+		{
+			fontSize = 20,
+			fontStyle = FontStyle.Bold
+		};
+		GUILayout.Label("NewTestCase", titleStyle);
+		newTestCaseName = GUILayout.TextField(newTestCaseName);
+		
+		GUILayout.Button("Add");
+		GUILayout.EndArea();
 	}
 }
