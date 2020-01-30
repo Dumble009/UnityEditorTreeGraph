b0d9d85021d0945694d6f25c951a898b0ae76bdd
Dumble009 <daichi19990821@gmail.com>
2020-01-30 15:10:14 +0900

    Change SubNode Color

--- TreeGraph/GraphNodes/SubNode.cs
+++ TreeGraph/GraphNodes/SubNode.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 [CreateNodeMenu("")]
+[NodeTint("#aaffaa")]
 public class SubNode : Node, IBTGraphNode
 {
     public string nodeName;
