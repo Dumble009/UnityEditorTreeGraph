71271d8440ae850ff3534cb4c8a0443876d7761d
Dumble009 <daichi19990821@gmail.com>
2020-02-07 23:38:57 +0900

    add ITestTreeGraphNode interface

---
+++ TreeGraph/GraphNodes/TestCodeNodes/ITestTreeGraphNode.cs
@@ -0,0 +1,10 @@
+﻿using XNode;
+using System.Collections.Generic;
+public interface ITestTreeGraphNode
+{
+	string GetNodeName();
+	void SetNodeName(string name);
+	void InheritFrom(Node original);
+	CodeTemplateParameterHolder GetParameterHolder();
+	string GetKey();
+}
