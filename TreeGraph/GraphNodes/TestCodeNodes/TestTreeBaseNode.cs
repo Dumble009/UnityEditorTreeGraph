e341aaaf8606be6e6856b8a2e30155a58bb101de
Dumble009 <daichi19990821@gmail.com>
2020-02-12 23:39:06 +0900

    Add TestTreeBaseNode

---
+++ TreeGraph/GraphNodes/TestCodeNodes/TestTreeBaseNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class TestTreeBaseNode : Node, ITestTreeGraphNode
+{
+	public string nodeName;
+	[Input(ShowBackingValue.Never, ConnectionType.Multiple, TypeConstraint.None, false)] public string input;
+	[Output(ShowBackingValue.Never, ConnectionType.Override, false)] public string output;
+
+	public virtual string GetKey()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public virtual string GetNodeName()
+	{
+		return nodeName;
+	}
+
+	public virtual CodeTemplateParameterHolder GetParameterHolder()
+	{
+		CodeTemplateParameterHolder holder = new CodeTemplateParameterHolder();
+		holder.SetParameter("NodeName", nodeName);
+
+		return holder;
+	}
+
+	public virtual void InheritFrom(Node original)
+	{
+		if (original is TestTreeBaseNode b)
+		{
+			this.nodeName = b.nodeName;
+		}
+	}
+
+	public virtual void SetNodeName(string name)
+	{
+		nodeName = name;
+	}
+}
