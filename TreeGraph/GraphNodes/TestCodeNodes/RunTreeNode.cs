a10309813ceea51bfb27a4dd0e0b8c7d7ac80a20
Dumble009 <daichi19990821@gmail.com>
2020-02-12 23:43:37 +0900

    Implement GetParameterHolder of RunTreeNode

--- TreeGraph/GraphNodes/TestCodeNodes/RunTreeNode.cs
+++ TreeGraph/GraphNodes/TestCodeNodes/RunTreeNode.cs
@@ -5,5 +5,12 @@ using XNode;
 
 public class RunTreeNode : TestTreeBaseNode
 {
+	public string count;
 
+	public override CodeTemplateParameterHolder GetParameterHolder()
+	{
+		CodeTemplateParameterHolder holder = base.GetParameterHolder();
+		holder.SetParameter("Count", count);
+		return holder;
+	}
 }
