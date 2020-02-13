89a8a95b43a2e5b1802fd5e14e1013bbd64fc260
Dumble009 <daichi19990821@gmail.com>
2020-02-13 17:13:08 +0900

    Add CallValidation

---
+++ TreeGraph/GraphNodes/TestCodeNodes/CallValidation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallValidation : TestTreeBaseNode
+{
+	public string targetNodeName;
+	public override CodeTemplateParameterHolder GetParameterHolder()
+	{
+		CodeTemplateParameterHolder holder = base.GetParameterHolder();
+		holder.SetParameter("targetNodeName", targetNodeName);
+
+		return holder;
+	}
+}
