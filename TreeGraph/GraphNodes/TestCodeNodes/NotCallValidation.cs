9ac2137d22a31908fa9cebede5dd4b175f7306cd
Dumble009 <daichi19990821@gmail.com>
2020-02-22 16:16:12 +0900

    Add New Test Node NotCallValidation

---
+++ TreeGraph/GraphNodes/TestCodeNodes/NotCallValidation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[NodeTint("#ffff00")]
+[CreateNodeMenu("TestCodeTree/NotCallValidation")]
+public class NotCallValidation : TestTreeBaseNode
+{
+	public string targetNodeName;
+	public override CodeTemplateParameterHolder GetParameterHolder()
+	{
+		CodeTemplateParameterHolder holder = base.GetParameterHolder();
+		holder.SetParameter("targetNodeName", targetNodeName);
+
+		return holder;
+	}
+
+	public override string GetKey()
+	{
+		return "NotCallValidation";
+	}
+}
