19b3c8e08d5329b62c827303e24fa5c708eaa5a3
Dumble009 <daichi19990821@gmail.com>
2020-04-04 14:50:49 +0900

    Add NextSequenceNode

---
+++ TreeGraph/GraphNodes/NextSequenceNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateNodeMenu("Miscellaneous/NextSequence")]
+public class NextSequenceNode : BaseNode
+{
+
+	public override CodeTemplateParameterHolder GetParameterHolder()
+	{
+		CodeTemplateParameterHolder holder = new CodeTemplateParameterHolder();
+		holder.SetParameter("name", nodeName);
+
+		return holder;
+	}
+
+	public override string GetKey()
+	{
+		return "Success";
+	}
+}
